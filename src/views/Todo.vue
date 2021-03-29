646a827f9809b1743aef142ec905a99c4a360658
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2021-03-29 22:45:16 +0800

    110.03.29

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -6,19 +6,40 @@
 		<router-link to="/todo?filter=active" replace>Active</router-link> |
 		<router-link :to="{name:'Todo' , query:{filter:'done'}}" replace>Done</router-link> |
 
-		<p>show: {{ filter}}</p>
+		<!-- <p>show: {{ filter}}</p>
 		<div>
 			{{list}}
-		</div>
+		</div> -->
+		<ul>
+			<!-- <li v-for="item of list" :key="item.tId+''+item.todo.content" > -->
+			<!-- <li v-for="item of list" :key="item.tId">
+				<template v-if="edit !== item.tId">
+					<input type="checkbox" v-model="item.todo.done" >
+					{{item.todo.content}}
+				</template>
+				<template v-else>
+					<input type="text" v-model="item.todo.content">
+				</template>				
+			</li> -->
+			<TodoItem 
+				v-for="item of list" 
+				:key="item.tId" 
+				:todo="item.todo" 
+				:edit="item.tId === edit "
+				@check=" value => checkHandler(item.tId , value)"
+			/>
+		</ul>
 	</div>
 </template>
 
 
 <script>
+import TodoItem from '../components/TodoItem'
 export default {
 	data() {
 		return {
-		filter: "all", //all , active, done
+			filter: "all", //all , active, done
+			edit:null,
 		};
 	},
 	computed: {
@@ -27,9 +48,26 @@ export default {
 		},
 	},
 	watch:{
-		$route: function(route){
-			// check query
-			this.filter = route.query.filter || 'all'
+		// $route: function(route){
+		// 	// check query
+		// 	this.filter = route.query.filter || 'all'
+		// }
+
+		// immediate 網址改變，內容馬上變
+		// check qrery => redirect
+		$route:{
+			immediate:true,
+			handler:function(route){
+				this.filter = route.query.filter || 'all'
+			}
+		}
+	},
+	components:{
+		TodoItem
+	},
+	methods:{
+		checkHandler(tId ,value){
+			console.log(tId , value);
 		}
 	}
 };
